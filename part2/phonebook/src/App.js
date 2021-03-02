a4ef77e37775b547df0539a23d3b0c3d44cf84b2
poc-sleepy <mattari.honey.latte@gmail.com>
2021-03-03 01:38:52 +0900

    :sparkles: [part2] 演習問題2.6実施

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -1,22 +1,39 @@
 import React, { useState } from 'react';
 
+const Person = ({ person }) => <p>{person.name}</p>;
+
 const App = () => {
   const [persons, setPersons] = useState([{ name: 'Arto Hellas' }]);
   const [newName, setNewName] = useState('');
 
+  const handleFromSubmit = (event) => {
+    event.preventDefault();
+    const personObject = {
+      name: newName,
+    };
+    setPersons(persons.concat(personObject));
+    setNewName('');
+  };
+
+  const handleNameChange = (event) => {
+    setNewName(event.target.value);
+  };
+
   return (
     <div>
       <h2>Phonebook</h2>
-      <form>
+      <form onSubmit={handleFromSubmit}>
         <div>
-          name: <input />
+          name: <input value={newName} onChange={handleNameChange} />
         </div>
         <div>
           <button type="submit">add</button>
         </div>
       </form>
       <h2>Numbers</h2>
-      ...
+      {persons.map((person) => (
+        <Person key={person.name} person={person} />
+      ))}
     </div>
   );
 };
