607cadbc926a19051ef0128d31cbc9e79fc6b874
poc-sleepy <mattari.honey.latte@gmail.com>
2021-03-04 22:27:12 +0900

    :sparkles: [part2] 演習2.9, 2-10を実施。

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -1,5 +1,45 @@
 import React, { useState } from 'react';
 
+const Filter = ({ query, onChange }) => {
+  return (
+    <>
+      filter shown with <input value={query} onChange={onChange} />
+    </>
+  );
+};
+
+const PersonForm = ({
+  onSubmit,
+  newName,
+  newNameOnChange,
+  newNumber,
+  newNumberOnChange,
+}) => {
+  return (
+    <form onSubmit={onSubmit}>
+      <div>
+        name: <input value={newName} onChange={newNameOnChange} />
+      </div>
+      <div>
+        number: <input value={newNumber} onChange={newNumberOnChange} />
+      </div>
+      <div>
+        <button type="submit">add</button>
+      </div>
+    </form>
+  );
+};
+
+const Persons = ({ persons }) => {
+  return (
+    <>
+      {persons.map((person) => (
+        <Person key={person.name} person={person} />
+      ))}
+    </>
+  );
+};
+
 const Person = ({ person }) => (
   <p>
     {person.name} {person.number}
@@ -7,9 +47,22 @@ const Person = ({ person }) => (
 );
 
 const App = () => {
-  const [persons, setPersons] = useState([{ name: 'Arto Hellas', number: '040-1234567'}]);
+  const [persons, setPersons] = useState([
+    { name: 'Arto Hellas', number: '040-123456' },
+    { name: 'Ada Lovelace', number: '39-44-5323523' },
+    { name: 'Dan Abramov', number: '12-43-234345' },
+    { name: 'Mary Poppendieck', number: '39-23-6423122' },
+  ]);
   const [newName, setNewName] = useState('');
   const [newNumber, setNewNumber] = useState('');
+  const [query, setQuery] = useState('');
+  const [usesFilter, setUsesFilter] = useState(false);
+
+  const personsToShow = usesFilter
+    ? persons.filter((person) =>
+        person.name.toLowerCase().includes(query.toLowerCase())
+      )
+    : persons;
 
   const handleFromSubmit = (event) => {
     event.preventDefault();
@@ -34,24 +87,28 @@ const App = () => {
     setNewNumber(event.target.value);
   };
 
+  const handleQueryChange = (event) => {
+    setQuery(event.target.value.trim());
+    setUsesFilter(Boolean(event.target.value.trim()));
+  };
+
   return (
     <div>
       <h2>Phonebook</h2>
-      <form onSubmit={handleFromSubmit}>
-        <div>
-          name: <input value={newName} onChange={handleNameChange} />
-        </div>
-        <div>
-          number: <input value={newNumber} onChange={handleNumberChange} />
-        </div>
-        <div>
-          <button type="submit">add</button>
-        </div>
-      </form>
-      <h2>Numbers</h2>
-      {persons.map((person) => (
-        <Person key={person.name} person={person} />
-      ))}
+
+      <Filter query={query} onChange={handleQueryChange} />
+
+      <h3>Add a new</h3>
+      <PersonForm
+        onSubmit={handleFromSubmit}
+        newName={newName}
+        newNameOnChange={handleNameChange}
+        newNumber={newNumber}
+        newNumberOnChange={handleNumberChange}
+      />
+
+      <h3>Numbers</h3>
+      <Persons persons={personsToShow} />
     </div>
   );
 };
