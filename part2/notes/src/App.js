926dcc1ef503a4a14f3e236a5b5788cd1059910d
poc-sleepy <mattari.honey.latte@gmail.com>
2021-03-06 14:22:22 +0900

    ✨ [part2] 2.c db.jsonの追加とフェッチ機能追加

--- part2/notes/src/App.js
+++ part2/notes/src/App.js
@@ -1,10 +1,20 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 import Note from './components/Note';
 
-const App = (props) => {
-  const [notes, setNotes] = useState(props.notes);
+const App = () => {
+  const [notes, setNotes] = useState([]);
   const [newNote, setNewNote] = useState('a new note...');
   const [showAll, setShowAll] = useState(true);
+
+  useEffect(() => {
+    console.log('effect');
+    axios.get('http://localhost:3001/notes').then((response) => {
+      console.log('promise fulfilled');
+      setNotes(response.data);
+    });
+  }, []);
+  console.log('render', notes.length, 'notes');
 
   const addNote = (event) => {
     event.preventDefault();
@@ -35,7 +45,7 @@ const App = (props) => {
       <h1>Notes</h1>
       <div>
         <button onClick={() => setShowAll(!showAll)}>
-          show {showAll ? 'important' : 'all' }
+          show {showAll ? 'important' : 'all'}
         </button>
       </div>
       <ul>
