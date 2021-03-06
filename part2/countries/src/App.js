1add6bd2f359607926f781004dbd2f4f17778be0
poc-sleepy <mattari.honey.latte@gmail.com>
2021-03-06 15:00:38 +0900

    ✨ [part2] countries 国の取得処理実装

--- part2/countries/src/App.js
+++ part2/countries/src/App.js
@@ -1,6 +1,17 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 
 const App = () => {
+  const [countries, setCountries] = useState([]);
+
+  useEffect(() => {
+    axios.get('https://restcountries.eu/rest/v2/all').then((response) => {
+      setCountries(response.data);
+    });
+  }, []);
+
+  console.log('countries',countries);
+
   return <div>Hello</div>;
 };
 
