750997817285563feff313a95ad36621cc255ee7
poc-sleepy <mattari.honey.latte@gmail.com>
2021-03-11 17:25:39 +0900

    🎨 [part2] [notes] exportの表記を簡略化

--- part2/notes/src/services/notes.js
+++ part2/notes/src/services/notes.js
@@ -15,8 +15,4 @@ const update = (id, newObject) => {
     .then((response) => response.data);
 };
 
-export default {
-  getAll: getAll,
-  create: create,
-  update: update,
-};
+export default { getAll, create, update };
