c63a8a1178a813855bb73a9ea2eea835ceba309e
poc-sleepy <mattari.honey.latte@gmail.com>
2021-03-11 15:30:22 +0900

    🎨 [part2] notes 2-d servicesモジュールに外出し

--- part2/notes/src/App.js
+++ part2/notes/src/App.js
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from 'react';
-import axios from 'axios';
 import Note from './components/Note';
+import noteService from './services/notes';
 
 const App = () => {
   const [notes, setNotes] = useState([]);
@@ -9,9 +9,9 @@ const App = () => {
 
   useEffect(() => {
     console.log('effect');
-    axios.get('http://localhost:3001/notes').then((response) => {
+    noteService.getAll().then((initialNotes) => {
       console.log('promise fulfilled');
-      setNotes(response.data);
+      setNotes(initialNotes);
     });
   }, []);
   console.log('render', notes.length, 'notes');
@@ -24,8 +24,8 @@ const App = () => {
       important: Math.random() < 0.5,
     };
 
-    axios.post('http://localhost:3001/notes', noteObject).then((response) => {
-      setNotes(notes.concat(response.data));
+    noteService.create(noteObject).then((returnedNote) => {
+      setNotes(notes.concat(returnedNote));
       setNewNote('');
     });
   };
@@ -40,8 +40,8 @@ const App = () => {
     const note = notes.find((n) => n.id === id);
     const changedNote = { ...note, important: !note.important };
 
-    axios.put(url, changedNote).then((response) => {
-      setNotes(notes.map((note) => (note.id !== id ? note : response.data)));
+    noteService.update(id, changedNote).then((returnedNote) => {
+      setNotes(notes.map((note) => (note.id !== id ? note : returnedNote)));
     });
   };
 
