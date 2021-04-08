1d0b3cdd7f613215069c760ccbe3dd19d00875e0
poc-sleepy <mattari.honey.latte@gmail.com>
2021-04-08 23:21:37 +0900

    ✨ [part9][patient] 演習9.19 実施

--- part9/patientor/src/types.ts
+++ part9/patientor/src/types.ts
@@ -1,6 +1,48 @@
-export type Entry = {
+interface BaseEntry {
+  id: string;
+  description: string;
+  date: string;
+  specialist: string;
+  diagnosisCodes?: Array<Diagnose['code']>;
+}
 
+export type Discharge = {
+  date: string;
+  criteria: string;
 };
+
+interface HospitalEntry extends BaseEntry {
+  type: 'Hospital';
+  discharge: Discharge;
+}
+
+export type SickLeave = {
+  startDate: string;
+  endDate: string;
+};
+
+interface OccupationalHealthcareEntry extends BaseEntry {
+  type: 'OccupationalHealthcare';
+  employerName: string;
+  sickLeave?: SickLeave;
+}
+
+export enum HealthCheckRating {
+  'Healthy' = 0,
+  'LowRisk' = 1,
+  'HighRisk' = 2,
+  'CriticalRisk' = 3,
+}
+
+interface HealthCheckEntry extends BaseEntry {
+  type: 'HealthCheck';
+  healthCheckRating: HealthCheckRating;
+}
+
+export type Entry =
+  | HospitalEntry
+  | OccupationalHealthcareEntry
+  | HealthCheckEntry;
 
 export enum Gender {
   Male = 'male',
