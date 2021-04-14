76d8bcf42d60455f1adf03cc94609bbea6f55518
poc-sleepy <mattari.honey.latte@gmail.com>
2021-04-15 00:26:08 +0900

    ✨ [part3][phonebook] 演習3.2まで

---
+++ part3/phonebook_back/src/index.ts
@@ -0,0 +1,31 @@
+import express from 'express';
+import { Person } from './types';
+
+const app = express();
+app.use(express.json());
+
+const persons: Person[] = [
+  { id: 1, name: 'Arto Hellas', number: '040-123456' },
+  { id: 2, name: 'Ada Lovelace', number: '39-44-5323523' },
+  { id: 3, name: 'Dan Abramov', number: '12-43-234345' },
+  { id: 4, name: 'Mary Poppendieck', number: '39-23-6423122' },
+];
+
+app.get('/ping', (_request, response) => {
+  response.send('pong');
+});
+
+app.get('/info', (_request, response) => {
+  response.send(
+    `<p>Phonebook has info for ${persons.length} people</p><p>${new Date()}</p>`
+  );
+});
+
+app.get('/api/persons', (_request, response) => {
+  response.json(persons);
+});
+
+const PORT = 3001;
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
