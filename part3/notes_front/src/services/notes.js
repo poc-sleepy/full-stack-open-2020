ca815a68a9d0043c4b72917f7244daa84f3d90ad
poc-sleepy <mattari.honey.latte@gmail.com>
2021-04-16 00:12:52 +0900

    🎉 [part3][notes_front] initial commit

---
+++ part3/notes_front/src/services/notes.js
@@ -0,0 +1,18 @@
+import axios from 'axios';
+const baseUrl = 'http://localhost:3001/api/notes';
+
+const getAll = () => {
+  return axios.get(baseUrl).then((response) => response.data);
+};
+
+const create = (newObject) => {
+  return axios.post(baseUrl, newObject).then((response) => response.data);
+};
+
+const update = (id, newObject) => {
+  return axios
+    .put(`${baseUrl}/${id}`, newObject)
+    .then((response) => response.data);
+};
+
+export default { getAll, create, update };
