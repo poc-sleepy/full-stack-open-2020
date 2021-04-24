d4e5a87364c66d0c8c00583b79d2fb209b955cb9
poc-sleepy <mattari.honey.latte@gmail.com>
2021-04-24 18:45:21 +0900

    ✨ [3-c] noteのリスト取得コードを追加

--- part3/notes_back/src/mongo.ts
+++ part3/notes_back/src/mongo.ts
@@ -36,3 +36,10 @@ void note.save().then((_result) => {
   console.log('note saved!');
   void mongoose.connection.close();
 });
+
+void Note.find({}).then((result) => {
+  result.forEach((note) => {
+    console.log(note);
+  });
+  void mongoose.connection.close();
+});
