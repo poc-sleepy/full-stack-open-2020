5238cc7ca58471a265596a6d262edf4fc5185271
poc-sleepy <mattari.honey.latte@gmail.com>
2021-05-02 01:16:13 +0900

    🎨 [ex.4.1] コンソールログをロガー経由に変更

--- part4/bloglist_back/src/index.ts
+++ part4/bloglist_back/src/index.ts
@@ -1,8 +1,9 @@
 import { config } from './utils/config';
 
 import { app } from './app';
+import { logger } from './utils/logger';
 
 const PORT = config.PORT;
 app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
+  logger.info(`Server running on port ${PORT}`);
 });
