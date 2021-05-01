0ae697b191ba309fa61205ac29282d3cdb9a900e
poc-sleepy <mattari.honey.latte@gmail.com>
2021-05-02 01:08:02 +0900

    🎨 [ex.4.1] appとindexを分離

--- part4/bloglist_back/src/index.ts
+++ part4/bloglist_back/src/index.ts
@@ -1,27 +1,6 @@
-import express from 'express';
-import cors from 'cors';
-import mongoose from 'mongoose';
-
 import { config } from './utils/config';
-import { controllers } from './controllers/blogs';
 
-const app = express();
-
-const mongoUrl = config.MONGODB_URI;
-if (mongoUrl === undefined) {
-  throw new Error('Environment variable MONGODB_URI is not given.');
-}
-void mongoose.connect(mongoUrl, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-  useFindAndModify: false,
-  useCreateIndex: true,
-});
-
-app.use(cors());
-app.use(express.json());
-
-app.use('/api/blogs', controllers.blogsRouter);
+import { app } from './app';
 
 const PORT = config.PORT;
 app.listen(PORT, () => {
