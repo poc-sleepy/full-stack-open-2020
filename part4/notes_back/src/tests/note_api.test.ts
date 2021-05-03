e80f60eba62290162fb12a5779c629b44b2967c5
poc-sleepy <mattari.honey.latte@gmail.com>
2021-05-03 20:05:22 +0900

    🎨 [4-b] beforeEachの初期データ投入コードを修正

--- part4/notes_back/src/tests/note_api.test.ts
+++ part4/notes_back/src/tests/note_api.test.ts
@@ -8,11 +8,10 @@ import { helper } from './test_helper';
 const api = supertest(app);
 
 beforeEach(async () => {
-  void (await Note.deleteMany({}));
-  let noteObject = new Note(helper.initialNotes[0]);
-  void (await noteObject.save());
-  noteObject = new Note(helper.initialNotes[1]);
-  void (await noteObject.save());
+  await Note.deleteMany({});
+  const noteObjects = helper.initialNotes.map((note) => new Note(note));
+  const promiseArray = noteObjects.map((note) => note.save());
+  await Promise.all(promiseArray);
 });
 
 test('notes are returned as json', async () => {
