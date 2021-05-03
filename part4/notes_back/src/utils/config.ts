39e958ce64c2ae9cef2f8098c04ddd86683608e2
poc-sleepy <mattari.honey.latte@gmail.com>
2021-05-03 14:03:31 +0900

    🎨 [4-b] cross-env導入と、テスト環境の分離

--- part4/notes_back/src/utils/config.ts
+++ part4/notes_back/src/utils/config.ts
@@ -3,7 +3,10 @@ import dotenv from 'dotenv';
 dotenv.config();
 
 const PORT = process.env.PORT;
-const MONGODB_URI = process.env.MONGODB_URI;
+const MONGODB_URI =
+  process.env.NODE_ENV === 'test'
+    ? process.env.TEST_MONGODB_URI
+    : process.env.MONGODB_URI;
 
 export const config = {
   MONGODB_URI,
