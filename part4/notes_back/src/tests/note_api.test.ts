cc59b71bb55b0028443b10a8728552ca6e622567
poc-sleepy <mattari.honey.latte@gmail.com>
2021-05-03 18:23:51 +0900

    ✅ [4-b] post用のテストを追加

--- part4/notes_back/src/tests/note_api.test.ts
+++ part4/notes_back/src/tests/note_api.test.ts
@@ -2,7 +2,7 @@ import mongoose from 'mongoose';
 import supertest from 'supertest';
 import { app } from '../app';
 import { Note } from '../models/note';
-import { NoteType } from '../utils/types';
+import { NewNote, NoteType } from '../utils/types';
 
 const api = supertest(app);
 
@@ -49,6 +49,38 @@ test('a specific note is within the returned notes', async () => {
   const notes: NoteType[] = response.body;
   const contents = notes.map((r) => r.content);
   expect(contents).toContain('Browser can execute only Javascript');
+});
+
+test('a valid note can be added', async () => {
+  const newNote: NewNote = {
+    content: 'async/await simplifies making async calls',
+    important: true,
+  };
+
+  await api
+    .post('/api/notes')
+    .send(newNote)
+    .expect(200)
+    .expect('Content-Type', /application\/json/);
+
+  const response = await api.get('/api/notes');
+  // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
+  const notes: NoteType[] = response.body;
+  const contents = notes.map((r) => r.content);
+
+  expect(notes).toHaveLength(initialNotes.length + 1);
+  expect(contents).toContain('async/await simplifies making async calls');
+});
+
+test('note without content is not added', async () => {
+  const newNote = {
+    important: true,
+  };
+  await api.post('/api/notes').send(newNote).expect(400);
+
+  const response = await api.get('/api/notes');
+
+  expect(response.body).toHaveLength(initialNotes.length);
 });
 
 afterAll(() => {
