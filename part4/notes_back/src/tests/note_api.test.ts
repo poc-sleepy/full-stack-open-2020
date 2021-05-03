3ca66647c60c1a121432f99151ee4239745a8b83
poc-sleepy <mattari.honey.latte@gmail.com>
2021-05-03 15:55:46 +0900

    ✅ [4-b] テストを追加

--- part4/notes_back/src/tests/note_api.test.ts
+++ part4/notes_back/src/tests/note_api.test.ts
@@ -1,6 +1,6 @@
 import mongoose from 'mongoose';
 import supertest from 'supertest';
-import {app} from '../app';
+import { app } from '../app';
 
 const api = supertest(app);
 
@@ -13,4 +13,16 @@ test('notes are returned as json', async () => {
 
 afterAll(() => {
   void mongoose.connection.close();
+});
+
+test('there are two notes', async () => {
+  const response = await api.get('/api/notes');
+
+  expect(response.body).toHaveLength(2);
+});
+
+test('the first note is about HTTP methods', async () => {
+  const response = await api.get('/api/notes');
+
+  expect(response.body[0].content).toBe('HTML is easy');
 });
