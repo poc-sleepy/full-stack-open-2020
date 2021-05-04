e0e9b710372eda9b8c387cd093bba9ada47e9e09
poc-sleepy <mattari.honey.latte@gmail.com>
2021-05-04 15:02:25 +0900

    ✅ [ex4.9] GETしたときのBlogのフォーマットを確認するテストを追加

--- part4/bloglist_back/src/tests/blog_api.test.ts
+++ part4/bloglist_back/src/tests/blog_api.test.ts
@@ -23,15 +23,13 @@ describe('when there is initially some blogs saved', () => {
 
   test('all blogs are returned', async () => {
     const response = await api.get('/api/blogs');
-
     expect(response.body).toHaveLength(helper.initialBlogs.length);
   });
 
   test('a specific blog is within the returned notes', async () => {
     const response = await api.get('/api/blogs');
-
     // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
-    const blogs: BlogType[] = response.body;
+    const blogs = response.body;
     const expectedBlog: BlogType = {
       _id: '5a422a851b54a676234d17f7',
       title: 'React patterns',
@@ -41,6 +39,20 @@ describe('when there is initially some blogs saved', () => {
       __v: 0,
     };
     expect(blogs).toContainEqual(expectedBlog);
+  });
+
+  test('a specific blog has correct properties', async () => {
+    const response = await api.get('/api/blogs');
+
+    // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
+    const blog = response.body[0];
+    expect(blog.id).toBeDefined();
+    expect(blog.title).toBeDefined();
+    expect(blog.author).toBeDefined();
+    expect(blog.url).toBeDefined();
+    expect(blog.likes).toBeDefined();
+    expect(blog._id).toBeUndefined();
+    expect(blog.__v).toBeUndefined();
   });
 });
 
