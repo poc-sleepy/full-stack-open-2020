209d9f4c2026ccc339e72cd99dca494904160213
poc-sleepy <mattari.honey.latte@gmail.com>
2021-05-04 13:58:55 +0900

    ✨ [ex.4.8] supertest, cross-envの導入、本番/テスト/開発の分離

--- part4/bloglist_back/src/utils/config.ts
+++ part4/bloglist_back/src/utils/config.ts
@@ -2,7 +2,10 @@ import dotenv from 'dotenv';
 
 dotenv.config();
 
-const MONGODB_URI = process.env.MONGODB_URI;
+const MONGODB_URI =
+  process.env.NODE_ENV === 'test'
+    ? process.env.TEST_MONGODB_URI
+    : process.env.MONGODB_URI;
 const PORT = process.env.PORT;
 
 export const config = {
