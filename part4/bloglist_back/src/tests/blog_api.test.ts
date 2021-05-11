cb5a583841dbc5be2eb996da44fa16d91852974b
poc-sleepy <mattari.honey.latte@gmail.com>
2021-05-11 15:17:02 +0900

    ✅ [ex.4.23] トークン認証にともなう修正およびケース追加

--- part4/bloglist_back/src/tests/blog_api.test.ts
+++ part4/bloglist_back/src/tests/blog_api.test.ts
@@ -3,10 +3,17 @@ import supertest from 'supertest';
 
 import { app } from '../app';
 import { Blog } from '../models/blog';
-import { BlogType } from '../utils/types';
+import { User } from '../models/user';
 import { helper } from './test_helper';
 
 const api = supertest(app);
+
+beforeAll(async () => {
+  // HACK: 本当はUser.notesを初期化するためbeforeEachに入れるのが正解
+  // 時間短縮のため今回はこっちに書く
+  await User.deleteMany({});
+  await User.insertMany(await helper.initialUsers);
+});
 
 beforeEach(async () => {
   await Blog.deleteMany({});
@@ -30,12 +37,24 @@ describe('when there is initially some blogs saved', () => {
     const response = await api.get('/api/blogs');
     // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
     const blogs = response.body;
-    const expectedBlog: BlogType = {
-      id: '5a422a851b54a676234d17f7',
-      title: 'React patterns',
-      author: 'Michael Chan',
-      url: 'https://reactpatterns.com/',
-      likes: 7,
+    const expectedUser = (await helper.initialUsers).find(
+      (user) => user._id === helper.initialBlogs[0].createdBy
+    );
+    if (expectedUser === undefined) {
+      throw new Error('Something in the testdata is wrong');
+    }
+
+    const expectedBlog = {
+      id: helper.initialBlogs[0]._id,
+      title: helper.initialBlogs[0].title,
+      author: helper.initialBlogs[0].author,
+      url: helper.initialBlogs[0].url,
+      likes: helper.initialBlogs[0].likes,
+      createdBy: {
+        id: expectedUser._id,
+        username: expectedUser.username,
+        name: expectedUser.name,
+      },
     };
     expect(blogs).toContainEqual(expectedBlog);
   });
@@ -50,6 +69,7 @@ describe('when there is initially some blogs saved', () => {
     expect(blog.author).toBeDefined();
     expect(blog.url).toBeDefined();
     expect(blog.likes).toBeDefined();
+    expect(blog.createdBy).toBeDefined();
     expect(blog._id).toBeUndefined();
     expect(blog.__v).toBeUndefined();
   });
@@ -57,6 +77,13 @@ describe('when there is initially some blogs saved', () => {
 
 describe('addition of a new blog', () => {
   test('succeeds with valid data', async () => {
+    const user = await helper.loginUser(
+      api,
+      (
+        await helper.initialUsers
+      )[0].username
+    );
+
     const newBlog = {
       title: 'fullstack open',
       author: 'Helsinki Univ.',
@@ -66,6 +93,7 @@ describe('addition of a new blog', () => {
 
     await api
       .post('/api/blogs')
+      .set('Authorization', `Bearer ${user.token}`)
       .send(newBlog)
       .expect(201)
       .expect('Content-Type', /application\/json/);
@@ -76,12 +104,24 @@ describe('addition of a new blog', () => {
     const addedBlog = blogsAtEnd.find((blog) => blog.title === newBlog.title);
     expect(addedBlog).toBeDefined();
     if (addedBlog !== undefined) {
-      // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
-      expect(addedBlog).toEqual({ id: addedBlog.id, ...newBlog });
+      expect(addedBlog).toEqual({
+        // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
+        id: addedBlog.id,
+        ...newBlog,
+        // HACK: 今回は無理やりパスさせるようIdに変換しているが、本来はtoJSONにてstringへの変換が必要
+        createdBy: mongoose.Types.ObjectId(user._id),
+      });
     }
   });
 
   test('succeeds without likes', async () => {
+    const user = await helper.loginUser(
+      api,
+      (
+        await helper.initialUsers
+      )[0].username
+    );
+
     const newBlog = {
       title: 'fullstack open',
       author: 'Helsinki Univ.',
@@ -90,6 +130,7 @@ describe('addition of a new blog', () => {
 
     await api
       .post('/api/blogs')
+      .set('Authorization', `Bearer ${user.token}`)
       .send(newBlog)
       .expect(201)
       .expect('Content-Type', /application\/json/);
@@ -100,12 +141,25 @@ describe('addition of a new blog', () => {
     const addedBlog = blogsAtEnd.find((blog) => blog.title === newBlog.title);
     expect(addedBlog).toBeDefined();
     if (addedBlog !== undefined) {
-      // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
-      expect(addedBlog).toEqual({ id: addedBlog.id, ...newBlog, likes: 0 });
+      expect(addedBlog).toEqual({
+        // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
+        id: addedBlog.id,
+        ...newBlog,
+        likes: 0,
+        // HACK: 今回は無理やりパスさせるようIdに変換しているが、本来はtoJSONにてstringへの変換が必要
+        createdBy: mongoose.Types.ObjectId(user._id),
+      });
     }
   });
 
   test('succeeds without title', async () => {
+    const user = await helper.loginUser(
+      api,
+      (
+        await helper.initialUsers
+      )[0].username
+    );
+
     const newBlog = {
       author: 'Helsinki Univ.',
       url: 'https://fullstackopen.com/en/',
@@ -113,6 +167,7 @@ describe('addition of a new blog', () => {
 
     await api
       .post('/api/blogs')
+      .set('Authorization', `Bearer ${user.token}`)
       .send(newBlog)
       .expect(201)
       .expect('Content-Type', /application\/json/);
@@ -123,12 +178,25 @@ describe('addition of a new blog', () => {
     const addedBlog = blogsAtEnd.find((blog) => blog.url === newBlog.url);
     expect(addedBlog).toBeDefined();
     if (addedBlog !== undefined) {
-      // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
-      expect(addedBlog).toEqual({ id: addedBlog.id, ...newBlog, likes: 0 });
+      expect(addedBlog).toEqual({
+        // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
+        id: addedBlog.id,
+        ...newBlog,
+        likes: 0,
+        // HACK: 今回は無理やりパスさせるようIdに変換しているが、本来はtoJSONにてstringへの変換が必要
+        createdBy: mongoose.Types.ObjectId(user._id),
+      });
     }
   });
 
   test('succeeds without url', async () => {
+    const user = await helper.loginUser(
+      api,
+      (
+        await helper.initialUsers
+      )[0].username
+    );
+
     const newBlog = {
       title: 'fullstack open',
       author: 'Helsinki Univ.',
@@ -136,6 +204,7 @@ describe('addition of a new blog', () => {
 
     await api
       .post('/api/blogs')
+      .set('Authorization', `Bearer ${user.token}`)
       .send(newBlog)
       .expect(201)
       .expect('Content-Type', /application\/json/);
@@ -146,18 +215,51 @@ describe('addition of a new blog', () => {
     const addedBlog = blogsAtEnd.find((blog) => blog.title === newBlog.title);
     expect(addedBlog).toBeDefined();
     if (addedBlog !== undefined) {
-      // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
-      expect(addedBlog).toEqual({ id: addedBlog.id, ...newBlog, likes: 0 });
+      expect(addedBlog).toEqual({
+        // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
+        id: addedBlog.id,
+        ...newBlog,
+        likes: 0,
+        // HACK: 今回は無理やりパスさせるようIdに変換しているが、本来はtoJSONにてstringへの変換が必要
+        createdBy: mongoose.Types.ObjectId(user._id),
+      });
     }
   });
 
+  test('fails without auth', async () => {
+    const newBlog = {
+      title: 'fullstack open',
+      author: 'Helsinki Univ.',
+      url: 'https://fullstackopen.com/en/',
+      likes: 2,
+    };
+
+    await api.post('/api/blogs').send(newBlog).expect(401);
+
+    const blogsAtEnd = await helper.blogsInDb();
+    expect(blogsAtEnd).toHaveLength(helper.initialBlogs.length);
+  });
+
   test('fails without title and url', async () => {
+    const user = (await helper.initialUsers)[0];
+    // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
+    const token = (
+      await api
+        .post('/api/login')
+        .send({ ...user, password: 'secret' })
+        .expect(200)
+    ).body.token;
+
     const newBlog = {
       author: 'Helsinki Univ.',
       likes: 5,
     };
 
-    await api.post('/api/blogs').send(newBlog).expect(400);
+    await api
+      .post('/api/blogs')
+      .set('Authorization', `Bearer ${token}`)
+      .send(newBlog)
+      .expect(400);
 
     const blogsAtEnd = await helper.blogsInDb();
     expect(blogsAtEnd).toHaveLength(helper.initialBlogs.length);
@@ -166,7 +268,7 @@ describe('addition of a new blog', () => {
 
 describe('updation of a blog', () => {
   test('succeeds with valid data', async () => {
-    const blogsAtStart: BlogType[] = await helper.blogsInDb();
+    const blogsAtStart = await helper.blogsInDb();
     const blogToUpdate = blogsAtStart[0];
 
     const toBeBlog = {
@@ -210,10 +312,20 @@ describe('updation of a blog', () => {
 
 describe('deletion of a blog', () => {
   test('succeeds with status code 204 if id is valid', async () => {
+    const user = await helper.loginUser(
+      api,
+      (
+        await helper.initialUsers
+      )[0].username
+    );
+
     const blogsAtStart = await helper.blogsInDb();
     const blogToDelete = blogsAtStart[0];
 
-    await api.delete(`/api/blogs/${blogToDelete.id}`).expect(204);
+    await api
+      .delete(`/api/blogs/${blogToDelete.id}`)
+      .set('Authorization', `Bearer ${user.token}`)
+      .expect(204);
 
     const blogsAtEnd = await helper.blogsInDb();
     expect(blogsAtEnd).toHaveLength(helper.initialBlogs.length - 1);
@@ -221,9 +333,52 @@ describe('deletion of a blog', () => {
     expect(blogsAtEnd).not.toContainEqual(blogToDelete);
   });
 
+  test('fails by not-owner user', async () => {
+    const user = await helper.loginUser(
+      api,
+      (
+        await helper.initialUsers
+      )[1].username
+    );
+
+    const blogsAtStart = await helper.blogsInDb();
+    const blogToDelete = blogsAtStart[0];
+
+    await api
+      .delete(`/api/blogs/${blogToDelete.id}`)
+      .set('Authorization', `Bearer ${user.token}`)
+      .expect(401);
+
+    const blogsAtEnd = await helper.blogsInDb();
+    expect(blogsAtEnd).toHaveLength(helper.initialBlogs.length);
+
+    expect(blogsAtEnd).toContainEqual(blogToDelete);
+  });
+
   test('fails with statuscode 404 if blog does not exist', async () => {
+    const user = await helper.loginUser(
+      api,
+      (
+        await helper.initialUsers
+      )[0].username
+    );
+
     const validNonexistingId = await helper.nonExistingId();
-    await api.delete(`/api/blogs/${validNonexistingId}`).expect(404);
+    await api
+      .delete(`/api/blogs/${validNonexistingId}`)
+      .set('Authorization', `Bearer ${user.token}`)
+      .expect(404);
+  });
+
+  test('fails without auth', async () => {
+    const blogsAtStart = await helper.blogsInDb();
+    const blogToDelete = blogsAtStart[0];
+
+    await api.delete(`/api/blogs/${blogToDelete.id}`).expect(401);
+
+    const blogsAtEnd = await helper.blogsInDb();
+    expect(blogsAtEnd).toHaveLength(helper.initialBlogs.length);
+    expect(blogsAtEnd).toContainEqual(blogToDelete);
   });
 });
 
--- part4/bloglist_back/src/tests/test_helper.ts
+++ part4/bloglist_back/src/tests/test_helper.ts
@@ -1,32 +1,34 @@
 import bcrypt from 'bcrypt';
+import supertest from 'supertest';
 
 import { Blog } from '../models/blog';
 import { User } from '../models/user';
 
-export type MongoBlogType = {
+export type TestBlogType = {
   _id: string;
   title: string;
   author: string;
   url: string;
   likes: number;
-  __v?: number;
+  createdBy: string;
 };
 
-const initialBlogs: MongoBlogType[] = [
+const initialBlogs: TestBlogType[] = [
   {
     _id: '5a422a851b54a676234d17f7',
     title: 'React patterns',
     author: 'Michael Chan',
     url: 'https://reactpatterns.com/',
     likes: 7,
+    createdBy: '609769e9208b4d3728dc3557',
   },
   {
     _id: '5a422aa71b54a676234d17f8',
     title: 'Go To Statement Considered Harmful',
     author: 'Edsger W. Dijkstra',
-    url:
-      'http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html',
+    url: 'http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html',
     likes: 5,
+    createdBy: '6097cffa013ba926541808df',
   },
   {
     _id: '5a422b3a1b54a676234d17f9',
@@ -34,6 +36,7 @@ const initialBlogs: MongoBlogType[] = [
     author: 'Edsger W. Dijkstra',
     url: 'http://www.cs.utexas.edu/~EWD/transcriptions/EWD08xx/EWD808.html',
     likes: 12,
+    createdBy: '609769e9208b4d3728dc3557',
   },
   {
     _id: '5a422b891b54a676234d17fa',
@@ -41,14 +44,15 @@ const initialBlogs: MongoBlogType[] = [
     author: 'Robert C. Martin',
     url: 'http://blog.cleancoder.com/uncle-bob/2017/05/05/TestDefinitions.html',
     likes: 10,
+    createdBy: '609769e9208b4d3728dc3557',
   },
   {
     _id: '5a422ba71b54a676234d17fb',
     title: 'TDD harms architecture',
     author: 'Robert C. Martin',
-    url:
-      'http://blog.cleancoder.com/uncle-bob/2017/03/03/TDD-Harms-Architecture.html',
+    url: 'http://blog.cleancoder.com/uncle-bob/2017/03/03/TDD-Harms-Architecture.html',
     likes: 0,
+    createdBy: '6097716d976d7012184fb65b',
   },
   {
     _id: '5a422bc61b54a676234d17fc',
@@ -56,6 +60,7 @@ const initialBlogs: MongoBlogType[] = [
     author: 'Robert C. Martin',
     url: 'http://blog.cleancoder.com/uncle-bob/2016/05/01/TypeWars.html',
     likes: 2,
+    createdBy: '6097cffa013ba926541808df',
   },
 ];
 
@@ -73,25 +78,72 @@ const nonExistingId = async () => {
   return id;
 };
 
-const getInitialUsers = async () => {
-  const passwordHash = await bcrypt.hash('sekret', 10);
-  const passwordHash2 = await bcrypt.hash('secret', 10);
-  const initialUsers = [
-    { username: 'root', name: 'Root User', passwordHash },
-    { username: 'admin', name: 'Administrator', passwordHash2 },
+export type TestUserType = {
+  _id: string;
+  username: string;
+  name: string;
+  passwordHash: string;
+  blogs: string[];
+};
+
+const initialUsers = (async () => {
+  const passwordHash = await bcrypt.hash('secret', 10);
+  const initialUsers: TestUserType[] = [
+    {
+      _id: '609769e9208b4d3728dc3557',
+      username: 'root',
+      name: 'Root User',
+      passwordHash,
+      blogs: [
+        '5a422a851b54a676234d17f7',
+        '5a422b3a1b54a676234d17f9',
+        '5a422b891b54a676234d17fa',
+      ],
+    },
+    {
+      _id: '6097716d976d7012184fb65b',
+      username: 'admin',
+      name: 'Administrator',
+      passwordHash,
+      blogs: ['5a422ba71b54a676234d17fb'],
+    },
+    {
+      _id: '6097cffa013ba926541808df',
+      username: 'kinaly',
+      name: 'Kinaly',
+      passwordHash,
+      blogs: ['5a422aa71b54a676234d17f8', '5a422bc61b54a676234d17fc'],
+    },
   ];
   return initialUsers;
-};
+})();
 
 const usersInDb = async () => {
   const users = await User.find({});
   return users.map((user) => user.toJSON());
 };
 
+const loginUser = async (
+  api: supertest.SuperTest<supertest.Test>,
+  username: string
+) => {
+  const user = (await helper.initialUsers).find((u) => u.username === username);
+  // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
+  const token: string = (
+    await api
+      .post('/api/login')
+      .send({ ...user, password: 'secret' })
+      .expect(200)
+  ).body.token;
+
+  return { ...user, token };
+};
+
 export const helper = {
   initialBlogs,
   blogsInDb,
   nonExistingId,
-  getInitialUsers,
+  initialUsers,
   usersInDb,
+  loginUser,
 };
