225f41492fa272ad4be5f1092b0145cdc5c1f2eb
poc-sleepy <mattari.honey.latte@gmail.com>
2021-05-15 19:36:51 +0900

    ✨ [5-b] Togglableの実装

---
+++ part5/notes_front/src/components/Togglable.tsx
@@ -0,0 +1,30 @@
+import React, { useState } from 'react';
+
+type Props = {
+  buttonLabel: string;
+};
+
+const Togglable: React.FC<Props> = (props) => {
+  const [visible, setVisible] = useState(false);
+
+  const hideWhenVisible = { display: visible ? 'none' : '' };
+  const showWhenVisible = { display: visible ? '' : 'none' };
+
+  const toggleVisibility = () => {
+    setVisible(!visible);
+  };
+
+  return (
+    <div>
+      <div style={hideWhenVisible}>
+        <button onClick={toggleVisibility}>{props.buttonLabel}</button>
+      </div>
+      <div style={showWhenVisible}>
+        {props.children}
+        <button onClick={toggleVisibility}>cancel</button>
+      </div>
+    </div>
+  );
+};
+
+export default Togglable;
