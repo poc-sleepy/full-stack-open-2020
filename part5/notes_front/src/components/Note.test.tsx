ce1622e97626934a213d01d841b906ac45be941f
poc-sleepy <mattari.honey.latte@gmail.com>
2021-05-18 23:53:04 +0900

    ✅ [5-c] toggleImpotantボタンのテスト追加

--- part5/notes_front/src/components/Note.test.tsx
+++ part5/notes_front/src/components/Note.test.tsx
@@ -1,26 +1,43 @@
 import React from 'react';
 import '@testing-library/jest-dom/extend-expect';
-import { render } from '@testing-library/react';
+import { fireEvent, render } from '@testing-library/react';
 import SingleNote from './Note';
 
 test('renders content', () => {
   const note = {
-    id: 'hogepiyo',
-    date: new Date().toISOString(),
+    id: 'hogehoge',
+    date: 'piyopiyo',
     content: 'Component testing is done with react-testing-library',
     important: true,
   };
 
+  const mockHandler = jest.fn();
+
   const component = render(
-    <SingleNote
-      note={note}
-      toggleImportance={() => {
-        return;
-      }}
-    />
+    <SingleNote note={note} toggleImportance={mockHandler} />
   );
 
   expect(component.container).toHaveTextContent(
     'Component testing is done with react-testing-library'
   );
+});
+
+test('clicking the button calls event handler once', () => {
+  const note = {
+    id: 'hogehoge',
+    date: 'piyopiyo',
+    content: 'Component testing is done with react-testing-library',
+    important: true,
+  };
+
+  const mockHandler = jest.fn();
+
+  const component = render(
+    <SingleNote note={note} toggleImportance={mockHandler} />
+  );
+
+  const button = component.getByText('make not important');
+  fireEvent.click(button);
+
+  expect(mockHandler.mock.calls).toHaveLength(1);
 });
