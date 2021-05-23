e8dc7d371c351df2f378554e4ddfb4214442b4f6
poc-sleepy <mattari.honey.latte@gmail.com>
2021-05-23 16:35:19 +0900

    ✅ [5-d] DB初期化およびユーザ作成のbeforeEachを追加

--- part5/notes_front/cypress/integration/note_app.spec.ts
+++ part5/notes_front/cypress/integration/note_app.spec.ts
@@ -1,5 +1,12 @@
 describe('Note app', function () {
   beforeEach(function () {
+    cy.request('POST', 'http://localhost:3001/api/testing/reset');
+    const user = {
+      name: 'Matti Luukkainen',
+      username: 'mluukkai',
+      password: 'salainen',
+    };
+    cy.request('POST', 'http://localhost:3001/api/users/', user);
     cy.visit('http://localhost:3000');
   });
 
