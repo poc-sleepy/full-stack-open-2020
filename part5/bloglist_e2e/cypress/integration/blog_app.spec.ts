d1b281de37d10fcf6cba0f0689ab89f2429eeffa
Kinaly-Hanaly <mattari.honey.latte@gmail.com>
2021-05-25 23:27:03 +0900

    ✅ [ex.5.18] ログインのテストを追加

--- part5/bloglist_e2e/cypress/integration/blog_app.spec.ts
+++ part5/bloglist_e2e/cypress/integration/blog_app.spec.ts
@@ -1,11 +1,36 @@
 describe('Blog app', function () {
   beforeEach(function () {
     cy.request('POST', 'http://localhost:3003/api/testing/reset');
+    const user = {username: 'poc', name: 'Poc Sleepy', password: 'password'}
+    cy.request('POST', 'http://localhost:3003/api/users', user).then(result => {
+      console.log(result.body);
+    });
     cy.visit('http://localhost:3000');
   });
 
   it('Login form is shown', function () {
     cy.get('button').contains('login');
+  });
+
+  describe('Login', function () {
+    it('succeeds with correct credentials', function () {
+      cy.contains('login').click();
+      cy.get('#username').type('poc');
+      cy.get('#password').type('password');
+      cy.get('#login_button').click();
+      cy.contains('Poc Sleepy logged in.');
+    });
+
+    it('fails with wrong credentials', function () {
+      cy.contains('login').click();
+      cy.get('#username').type('poc');
+      cy.get('#password').type('wrong');
+      cy.get('#login_button').click();
+      cy.should('not.contain', 'Poc Sleepy logged in.');
+      cy.get('#error_alert')
+        .should('contain','invalid username or password')
+        .and('have.css', 'background-color', 'rgb(253, 236, 234)');
+    });
   });
 });
 
