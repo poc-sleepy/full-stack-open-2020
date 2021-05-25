b26552555f382e248556c0007ff4a3c96e6b2236
poc-sleepy <mattari.honey.latte@gmail.com>
2021-05-25 21:22:45 +0900

    ✅ [ex.5.17] ログイン画面の表示テストを追加

---
+++ part5/bloglist_e2e/cypress/integration/blog_app.spec.ts
@@ -0,0 +1,12 @@
+describe('Blog app', function () {
+  beforeEach(function () {
+    cy.request('POST', 'http://localhost:3003/api/testing/reset');
+    cy.visit('http://localhost:3000');
+  });
+
+  it('Login form is shown', function () {
+    cy.get('button').contains('login');
+  });
+});
+
+export {};
