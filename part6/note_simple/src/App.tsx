634573d76a14fe4dd3d56b43da56964c4ffcdd85
poc-sleepy <mattari.honey.latte@gmail.com>
2021-05-28 19:45:21 +0900

    🎉 [6-a] notes_simpleの初期コミット

---
+++ part6/note_simple/src/App.tsx
@@ -0,0 +1,57 @@
+import React from 'react';
+import { createStore } from 'redux';
+
+type Note = {
+  id: string;
+  content: string;
+  date: string;
+  important: boolean;
+};
+
+const noteReducer = (
+  state: Note[] = [],
+  action: { type: string; data: any }
+) => {
+  if (action.type === 'NEW_NOTE') {
+    state.push(action.data);
+    return state;
+  }
+
+  return state;
+};
+
+const store = createStore(noteReducer);
+
+store.dispatch({
+  type: 'NEW_NOTE',
+  data: {
+    content: 'the app state is in redux store',
+    important: true,
+    id: 1,
+  },
+});
+
+store.dispatch({
+  type: 'NEW_NOTE',
+  data: {
+    content: 'state changes are made with actions',
+    important: false,
+    id: 2,
+  },
+});
+
+const App = () => {
+  return (
+    <div>
+      <ul>
+        {store.getState().map((note) => (
+          <li key={note.id}>
+            {note.content} <strong>{note.important ? 'important' : ''}</strong>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+};
+
+export default App;
