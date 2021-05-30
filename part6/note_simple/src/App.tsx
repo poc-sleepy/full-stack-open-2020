cfb43919a72d5c48f4a4cf9fdad2f94694183ec2
poc-sleepy <mattari.honey.latte@gmail.com>
2021-05-30 22:49:13 +0900

    ✨ [6-a] ノート追加と重要度変更処理を追加

--- part6/note_simple/src/App.tsx
+++ part6/note_simple/src/App.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import ReactDOM from 'react-dom';
 import { createStore } from 'redux';
 
 import { noteReducer } from './reducers/noteReducer';
@@ -30,12 +31,50 @@ store.dispatch({
   },
 });
 
+const generateId = () => Math.floor(Math.random() * 1000000);
+
+const createNote = (content: string) => {
+  return {
+    type: 'NEW_NOTE',
+    data: {
+      content,
+      important: false,
+      id: generateId(),
+    },
+  };
+};
+
+const toggleImportanceOf = (id: number) => {
+  return {
+    type: 'TOGGLE_IMPORTANCE',
+    data: { id },
+  };
+};
+
 const App = () => {
+  const addNote = (event: React.FormEvent<HTMLFormElement>) => {
+    console.log('called addNote');
+    event.preventDefault();
+    // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
+    const content: string = event.currentTarget.note.value;
+    event.currentTarget.note.value = '';
+    store.dispatch(createNote(content));
+  };
+
+  const toggleImportance = (id: number) => {
+    console.log('called toggleImportance');
+    store.dispatch(toggleImportanceOf(id));
+  };
+
   return (
     <div>
+      <form onSubmit={addNote}>
+        <input name="note" />
+        <button type="submit">add</button>
+      </form>
       <ul>
         {store.getState().map((note) => (
-          <li key={note.id}>
+          <li key={note.id} onClick={() => toggleImportance(Number(note.id))}>
             {note.content} <strong>{note.important ? 'important' : ''}</strong>
           </li>
         ))}
@@ -43,5 +82,12 @@ const App = () => {
     </div>
   );
 };
+
+const renderApp = () => {
+  ReactDOM.render(<App />, document.getElementById('root'));
+};
+
+renderApp();
+store.subscribe(renderApp);
 
 export default App;
