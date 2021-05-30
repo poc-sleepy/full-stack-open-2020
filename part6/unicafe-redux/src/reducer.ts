8307f11b276de5a1170725e8a163e5b31abdea8d
poc-sleepy <mattari.honey.latte@gmail.com>
2021-05-30 21:01:52 +0900

    ✨ [ex.6.1] unicafeの未実装部分を実装

--- part6/unicafe-redux/src/reducer.ts
+++ part6/unicafe-redux/src/reducer.ts
@@ -9,13 +9,13 @@ const counterReducer = (state = initialState, action: { type: any }) => {
   console.log(action);
   switch (action.type) {
     case 'GOOD':
-      return state;
+      return { ...state, good: state.good + 1 };
     case 'OK':
-      return state;
+      return { ...state, ok: state.ok + 1 };
     case 'BAD':
-      return state;
+      return { ...state, bad: state.bad + 1 };
     case 'ZERO':
-      return state;
+      return initialState;
     default:
       return state;
   }
