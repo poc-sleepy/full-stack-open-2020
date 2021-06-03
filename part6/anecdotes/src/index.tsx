b45a6469e32c4d22dc2a1b5c51657bfadae67a0e
poc-sleepy <mattari.honey.latte@gmail.com>
2021-06-03 21:46:35 +0900

    🔨 [ex.6.9] redux-devtoolを導入

--- part6/anecdotes/src/index.tsx
+++ part6/anecdotes/src/index.tsx
@@ -1,11 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { createStore } from 'redux';
 import { Provider } from 'react-redux';
-import App from './App';
-import { reducer } from './reducers/anecdoteReducer';
 
-const store = createStore(reducer);
+import App from './App';
+import { store } from './store';
 
 ReactDOM.render(
   <Provider store={store}>
