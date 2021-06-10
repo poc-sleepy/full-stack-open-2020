6355610d4b997f73f3d2ea0d52e5a28599a3868f
poc-sleepy <mattari.honey.latte@gmail.com>
2021-06-10 23:31:17 +0900

    ➕ [6-c] redux-thunk導入

--- part6/note_simple/src/store.ts
+++ part6/note_simple/src/store.ts
@@ -1,4 +1,5 @@
-import { combineReducers, createStore } from 'redux';
+import { applyMiddleware, combineReducers, createStore } from 'redux';
+import thunk from 'redux-thunk';
 import { composeWithDevTools } from 'redux-devtools-extension';
 import { filterReducer } from './reducers/filterReducer';
 import { noteReducer } from './reducers/noteReducer';
@@ -8,4 +9,7 @@ const reducer = combineReducers({
   filter: filterReducer,
 });
 
-export const store = createStore(reducer, composeWithDevTools());
+export const store = createStore(
+  reducer,
+  composeWithDevTools(applyMiddleware(thunk))
+);
