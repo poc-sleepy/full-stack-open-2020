43c3076d9a6b953eb04c1718a8c4d6298e1b89f2
poc-sleepy <mattari.honey.latte@gmail.com>
2021-06-22 21:18:16 +0900

    ✨ [7-a] Noteの詳細を見る機能を追加

--- part7/router_test/src/index.tsx
+++ part7/router_test/src/index.tsx
@@ -1,6 +1,12 @@
-import React from 'react';
+import React, { useState } from 'react';
 import ReactDOM from 'react-dom';
-import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link,
+  useParams,
+} from 'react-router-dom';
 
 const Home = () => (
   <div>
@@ -9,10 +15,42 @@ const Home = () => (
   </div>
 );
 
-const Notes = () => (
+type NoteType = {
+  id: number;
+  content: string;
+  important: boolean;
+  user: string;
+};
+
+type PropNotes = {
+  notes: NoteType[];
+};
+
+const Note = ({ notes }: PropNotes) => {
+  const id = useParams<{ id: string }>().id;
+  const note = notes.find((n) => n.id === Number(id));
+  if (note === undefined) return <></>;
+  return (
+    <div>
+      <h2>{note.content}</h2>
+      <div>{note.user}</div>
+      <div>
+        <strong>{note.important ? 'important' : ''}</strong>
+      </div>
+    </div>
+  );
+};
+
+const Notes = ({ notes }: PropNotes) => (
   <div>
-    {' '}
-    <h2>Notes</h2>{' '}
+    <h2>Notes</h2>
+    <ul>
+      {notes.map((note) => (
+        <li key={note.id}>
+          <Link to={`/notes/${note.id}`}>{note.content}</Link>
+        </li>
+      ))}
+    </ul>
   </div>
 );
 
@@ -24,6 +62,27 @@ const Users = () => (
 );
 
 const App = () => {
+  const [notes] = useState<NoteType[]>([
+    {
+      id: 1,
+      content: 'HTML is easy',
+      important: true,
+      user: 'Matti Luukkainen',
+    },
+    {
+      id: 2,
+      content: 'Browser can execute only Javascript',
+      important: false,
+      user: 'Matti Luukkainen',
+    },
+    {
+      id: 3,
+      content: 'Most important methods of HTTP-protocol are GET and POST',
+      important: true,
+      user: 'Arto Hellas',
+    },
+  ]);
+
   const padding = {
     padding: 5,
   };
@@ -31,31 +90,32 @@ const App = () => {
   return (
     <Router>
       <div>
-        <Link style={padding} to="/">
-          home
-        </Link>
-        <Link style={padding} to="/notes">
-          notes
-        </Link>
-        <Link style={padding} to="/users">
-          users
-        </Link>
-      </div>
+        <div>
+          <Link style={padding} to="/">
+            home
+          </Link>
+          <Link style={padding} to="/notes">
+            notes
+          </Link>
+          <Link style={padding} to="/users">
+            users
+          </Link>
+        </div>
 
-      <Switch>
-        <Route path="/notes">
-          <Notes />
-        </Route>
-        <Route path="/users">
-          <Users />
-        </Route>
-        <Route path="/">
-          <Home />
-        </Route>
-      </Switch>
-
-      <div>
-        <i>Note app, Department of Computer Science 2021</i>
+        <Switch>
+          <Route path="/notes/:id">
+            <Note notes={notes} />
+          </Route>
+          <Route path="/notes">
+            <Notes notes={notes} />
+          </Route>
+          <Route path="/users">
+            <Users />
+          </Route>
+          <Route path="/">
+            <Home />
+          </Route>
+        </Switch>
       </div>
     </Router>
   );
