43a7e8533947ab58acfbc01edfc173199876cdca
poc-sleepy <mattari.honey.latte@gmail.com>
2021-06-25 01:06:32 +0900

    🎉 [7-b] フォームアプリのfirst commit

---
+++ part7/form_hook_test/src/App.tsx
@@ -0,0 +1,39 @@
+import React, { useState } from 'react';
+
+const App = () => {
+  const [name, setName] = useState('');
+  const [born, setBorn] = useState('');
+  const [height, setHeight] = useState('');
+
+  return (
+    <div>
+      <form>
+        name:
+        <input
+          type="text"
+          value={name}
+          onChange={(event) => setName(event.target.value)}
+        />
+        <br />
+        birthdate:
+        <input
+          type="date"
+          value={born}
+          onChange={(event) => setBorn(event.target.value)}
+        />
+        <br />
+        height:
+        <input
+          type="number"
+          value={height}
+          onChange={(event) => setHeight(event.target.value)}
+        />
+      </form>
+      <div>
+        {name} {born} {height}
+      </div>
+    </div>
+  );
+};
+
+export default App;
