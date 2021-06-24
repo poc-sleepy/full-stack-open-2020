d8e1bb7b52ee0d9ca6fa2cad77f6bf1bd34310b1
poc-sleepy <mattari.honey.latte@gmail.com>
2021-06-25 01:10:24 +0900

    ♻️ [7-b] フック仕様に変更

--- part7/form_hook_test/src/App.tsx
+++ part7/form_hook_test/src/App.tsx
@@ -1,36 +1,44 @@
 import React, { useState } from 'react';
 
+const useField = (type: string) => {
+  const [value, setValue] = useState('');
+
+  const onChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setValue(event.target.value);
+  };
+
+  return {
+    type,
+    value,
+    onChange,
+  };
+};
+
 const App = () => {
-  const [name, setName] = useState('');
-  const [born, setBorn] = useState('');
-  const [height, setHeight] = useState('');
+  const name = useField('text');
+  const born = useField('date');
+  const height = useField('number');
+  // ...
 
   return (
     <div>
       <form>
         name:
-        <input
-          type="text"
-          value={name}
-          onChange={(event) => setName(event.target.value)}
-        />
+        <input type={name.type} value={name.value} onChange={name.onChange} />
         <br />
         birthdate:
-        <input
-          type="date"
-          value={born}
-          onChange={(event) => setBorn(event.target.value)}
-        />
+        <input type={born.type} value={born.value} onChange={born.onChange} />
         <br />
         height:
         <input
-          type="number"
-          value={height}
-          onChange={(event) => setHeight(event.target.value)}
+          type={height.type}
+          value={height.value}
+          onChange={height.onChange}
         />
+        <br />
       </form>
       <div>
-        {name} {born} {height}
+        {name.value} {born.value} {height.value}
       </div>
     </div>
   );
