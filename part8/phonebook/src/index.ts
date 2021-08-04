22a9ea24db4a17ae7e1c32c0eb1e8f39a904972e
poc-sleepy <mattari.honey.latte@gmail.com>
2021-08-05 00:08:48 +0900

    🎉 [8-a] initial commit

---
+++ part8/phonebook/src/index.ts
@@ -0,0 +1,55 @@
+import { ApolloServer } from 'apollo-server';
+import fs from 'fs';
+
+import { Resolvers, Person } from './generated/graphql';
+
+const persons: Person[] = [
+  {
+    name: 'Arto Hellas',
+    phone: '040-123543',
+    address: {
+      street: 'Tapiolankatu 5 A',
+      city: 'Espoo',
+    },
+    id: '3d594650-3436-11e9-bc57-8b80ba54c431',
+  },
+  {
+    name: 'Matti Luukkainen',
+    phone: '040-432342',
+    address: {
+      street: 'Malminkaari 10 A',
+      city: 'Helsinki',
+    },
+    id: '3d599470-3436-11e9-bc57-8b80ba54c431',
+  },
+  {
+    name: 'Venla Ruuska',
+    address: {
+      street: 'Nallemäentie 22 C',
+      city: 'Helsinki',
+    },
+    id: '3d599471-3436-11e9-bc57-8b80ba54c431',
+  },
+];
+
+const typeDefs = fs.readFileSync('./schema.graphql', { encoding: 'utf8' });
+
+const resolvers: Resolvers = {
+  Query: {
+    personCount: () => persons.length,
+    allPersons: () => persons,
+    findPerson: (_root, args) => {
+      const person = persons.find((p) => p.name === args.name);
+      return person === undefined ? null : person;
+    },
+  },
+};
+
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+});
+
+void server.listen().then(({ url }) => {
+  console.log(`Server ready at ${url}`);
+});
