0fe2646357e950d459eed923ae4c6ff0ed6661f2
poc-sleepy <mattari.honey.latte@gmail.com>
2021-09-07 01:31:37 +0900

    🎨  [10-b] RepositoryItemの表出情報を増やした

--- part10/rate-repository-app/src/components/RepositoryItem.tsx
+++ part10/rate-repository-app/src/components/RepositoryItem.tsx
@@ -1,7 +1,15 @@
 import React from 'react';
-import { Text } from 'react-native';
+import { Text, View } from 'react-native';
 import { Repository } from './types';
 
-export const RepositoryItem = (props: { repository: Repository }) => (
-  <Text>{props.repository.id}</Text>
+export const RepositoryItem = ({ repository }: { repository: Repository }) => (
+  <View>
+    <Text>Full Name: {repository.fullName}</Text>
+    <Text>Description: {repository.description}</Text>
+    <Text>Language: {repository.language}</Text>
+    <Text>Stars: {repository.stargazersCount}</Text>
+    <Text>Forks: {repository.forksCount}</Text>
+    <Text>Reviews: {repository.reviewCount}</Text>
+    <Text>Rating: {repository.ratingAverage}</Text>
+  </View>
 );
