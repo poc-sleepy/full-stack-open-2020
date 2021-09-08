8e295b34ec9514c8b64b0c848286b2bca72ccaa3
poc-sleepy <mattari.honey.latte@gmail.com>
2021-09-08 22:36:33 +0900

    ✨ [10-b] themeを実装

--- part10/rate-repository-app/src/components/Main.tsx
+++ part10/rate-repository-app/src/components/Main.tsx
@@ -1,20 +1,16 @@
 import React from 'react';
-import Constants from 'expo-constants';
-import { StyleSheet, View } from 'react-native';
-import RepositoryList from './RepositoryList';
 
-const styles = StyleSheet.create({
-  container: {
-    marginTop: Constants.statusBarHeight,
-    flexGrow: 1,
-    flexShrink: 1,
-  },
-});
+import { Text } from './Text';
 
 export const Main = () => {
   return (
-    <View style={styles.container}>
-      <RepositoryList />
-    </View>
+    <>
+      <Text>Simple text</Text>
+      <Text style={{ paddingBottom: 10 }}>Text with custom style</Text>
+      <Text fontWeight="bold" fontSize="subheading">
+        Bold subheading
+      </Text>
+      <Text color="textSecondary">Text with secondary color</Text>
+    </>
   );
 };
---
+++ part10/rate-repository-app/src/components/Text.tsx
@@ -0,0 +1,57 @@
+import React from 'react';
+import {
+  Text as NativeText,
+  StyleSheet,
+  StyleProp,
+  TextStyle,
+} from 'react-native';
+
+import { theme } from '../theme';
+
+const styles = StyleSheet.create({
+  text: {
+    color: theme.colors.textPrimary,
+    fontSize: theme.fontSizes.body,
+    fontFamily: theme.fonts.main,
+    fontWeight: theme.fontWeights.normal,
+  },
+  colorTextSecondary: {
+    color: theme.colors.textSecondary,
+  },
+  colorPrimary: {
+    color: theme.colors.primary,
+  },
+  fontSizeSubheading: {
+    fontSize: theme.fontSizes.subheading,
+  },
+  fontWeightBold: {
+    fontWeight: theme.fontWeights.bold,
+  },
+});
+
+interface TextProps {
+  color?: 'textSecondary' | 'primary';
+  fontSize?: 'subheading';
+  fontWeight?: 'bold';
+  style?: StyleProp<TextStyle>;
+  children: string;
+}
+
+export const Text = ({
+  color,
+  fontSize,
+  fontWeight,
+  style,
+  ...props
+}: TextProps) => {
+  const textStyle = [
+    styles.text,
+    color === 'textSecondary' && styles.colorTextSecondary,
+    color === 'primary' && styles.colorPrimary,
+    fontSize === 'subheading' && styles.fontSizeSubheading,
+    fontWeight === 'bold' && styles.fontWeightBold,
+    style,
+  ];
+
+  return <NativeText style={textStyle} {...props} />;
+};
