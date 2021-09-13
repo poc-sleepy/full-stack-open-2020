1d70f52e1be6a83adab4abec253a589202d6df3a
poc-sleepy <mattari.honey.latte@gmail.com>
2021-09-14 01:53:05 +0900

    ✨ [ex10.8] SigninFormを実装

--- part10/rate-repository-app/src/components/SignIn.tsx
+++ part10/rate-repository-app/src/components/SignIn.tsx
@@ -1,7 +1,62 @@
+import { Formik } from 'formik';
 import React from 'react';
+import { Button, View, StyleSheet } from 'react-native';
+import { theme } from '../theme';
+import { FormikTextInput } from './FormikTextInput';
 
-import { Text } from './Text';
+const initialValues = {
+  username: '',
+  password: '',
+};
+
+type SignInForm = {
+  onSubmit: any;
+};
+
+const SignInForm: React.FC<SignInForm> = ({ onSubmit }) => {
+  const styles = StyleSheet.create({
+    input: {
+      height: 40,
+      margin: 12,
+      borderWidth: 1,
+      padding: 10,
+      backgroundColor: theme.backgroundColors.white,
+    },
+  });
+
+  return (
+    <View>
+      <FormikTextInput
+        name="username"
+        placeholder="Username"
+        style={styles.input}
+      />
+      <FormikTextInput
+        name="password"
+        placeholder="Password"
+        style={styles.input}
+        secureTextEntry
+      />
+      <Button onPress={onSubmit} title="Sign In" />
+    </View>
+  );
+};
 
 export const SignIn = () => {
-  return <Text>The sign in view</Text>;
+  type onSubmitProps = {
+    username: string;
+    password: string;
+  };
+
+  const onSubmit = (values: onSubmitProps) => {
+    console.log(values);
+  };
+
+  return (
+    <>
+      <Formik initialValues={initialValues} onSubmit={onSubmit}>
+        {({ handleSubmit }) => <SignInForm onSubmit={handleSubmit} />}
+      </Formik>
+    </>
+  );
 };
